67e3399111618a7a87b3ec51d82f1de942d47c1a
sudo-u-Hung-NN <hung.nn184118@sis.hust.edu.vn>
2023-02-28 15:36:00 +0000

    Add indices files

--- download/tiny-imagenet.sh
+++ download/tiny-imagenet.sh
@@ -4,5 +4,8 @@ kaggle datasets download -d akash2sharma/tiny-imagenet
 # step 2: unzip & remove
 unzip tiny-imagenet.zip && rm -f tiny-imagenet.zip
 
+# step 3: remove abundant folder
+rm -r tiny-imagenet-200/tiny-imagenet-200
+
 # step 3: move to data folder
 mv tiny-imagenet-200 ../data/
